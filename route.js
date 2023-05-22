a4e1a21cc0c4857731df8b7b6ce28fdfc242bec4
Mpatil2805 <115547565+Mpatil2805@users.noreply.github.com>
2023-05-22 14:49:44 +0530

    Add files via upload

---
+++ route.js
@@ -0,0 +1,13 @@
+const express= require('express')
+const router=express.Router()
+const blockChainController = require('../controller/blockChainController')
+
+
+
+
+
+router.get("/assets",blockChainController.getCrypto)
+
+
+
+module.exports=router
